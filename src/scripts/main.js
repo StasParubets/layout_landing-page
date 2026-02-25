c5863863353b34b156eb4ebad6c4b4057a8914e6
Stanislav <parustas@gmail.com>
2026-02-25 13:39:16 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
+/* eslint-env browser */
 'use strict';
+
+const contactForm = document.getElementById('contact-form');
+
+if (contactForm) {
+  contactForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    contactForm.reset();
+    alert('Дякуємо! Ваше повідомлення надіслано.');
+  });
+}
